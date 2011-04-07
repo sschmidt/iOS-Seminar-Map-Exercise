69012d6f2dd854489aeb2de81f5906186c4ff0bc
Jan Knobloch <jan.knobloch@me.om>
2011-04-07 15:20:52 +0200

    distance works

---
+++ MyMapApp/MapAnnotation.h
@@ -0,0 +1,16 @@
+//
+//  MapAnnotation.h
+//  MyMapApp
+//
+//  Created by Jan Knobloch on 4/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MapAnnotation : NSObject {
+    
+}
+
+@end
---
+++ MyMapApp/MapAnnotation.m
@@ -0,0 +1,14 @@
+//
+//  MapAnnotation.m
+//  MyMapApp
+//
+//  Created by Jan Knobloch on 4/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapAnnotation.h"
+
+
+@implementation MapAnnotation
+
+@end
